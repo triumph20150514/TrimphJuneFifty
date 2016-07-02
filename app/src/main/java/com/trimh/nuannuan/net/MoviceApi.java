0765f858f5ac5c86b6ade8f3d5e86645120ee008
triumph20150514 <tx09trimph@163.com>
2016-07-02 10:57:33 +0800

    ggg

---
+++ app/src/main/java/com/trimh/nuannuan/net/MoviceApi.java
@@ -0,0 +1,31 @@
+package com.trimh.nuannuan.net;
+
+import com.trimh.nuannuan.bean.MoviceBean;
+
+import retrofit2.Retrofit;
+import rx.Observable;
+import rx.Subscriber;
+
+/**
+ * Project TrimphJuneFifty
+ * Package_name com.trimh.nuannuan.net
+ * Created by 涛 on 2016/6/29.
+ * Created by 涛  on 2016/6/29.
+ */
+public class MoviceApi {
+    NetService netService;
+
+    public MoviceApi() {
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://api.douban.com/v2/movie/").build();
+        netService = retrofit.create(NetService.class);
+    }
+
+    public void getMovice(Subscriber<MoviceBean> subscriber, int start, int cont) {
+        netService.getMovice(start, cont).subscribe(subscriber);
+
+
+    }
+
+
+}
